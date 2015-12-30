a098f99c75ccdabfd7302d9ef40578b7d82eac45
killer0124 <289856252@qq.com>
2015-12-30 19:59:03 +0800

    clean dev

--- WebRoot/src/devoops/js/theme/SetMinBlockHeight.js
+++
@@ -1,6 +0,0 @@
-//
-//  Function set min-height of window (required for this theme)
-//
-function SetMinBlockHeight(elem){
-	elem.css('min-height', window.innerHeight - 49);
-}
--- WebRoot/src/devoops/js/theme/drawGoogleChart.js
+++
@@ -1,9 +0,0 @@
-//
-// Helper for draw Google Chart
-//
-function drawGoogleChart(chart_data, chart_options, element, chart_type) {
-	// Function for visualize Google Chart
-	var data = google.visualization.arrayToDataTable(chart_data);
-	var chart = new chart_type(document.getElementById(element));
-	chart.draw(data, chart_options);
-}
